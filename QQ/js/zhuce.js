2af560f43fbfee6c1a365e94a00ed6567c43440f
吴子鑫 <1123116007@qq.com>
2017-10-20 20:10:28 +0800

    Add files via upload

---
+++ QQ/js/zhuce.js
@@ -0,0 +1,24 @@
+function check(){
+//	var password = document.getElementById('password'); 
+//	var repassword = document.getElementById('repassword);  
+//	    if(repassword.value!=password.value){
+//	        alert("两次输入的密码不一致");  
+//	    }
+alert('111');
+}
+	
+//	$('#btn').click(function(){
+//	    	var password1= $("password").value;
+//	        var repassword = $("repassword").value;  
+//	        var name = $("name").value;
+	        
+//		 $.ajax({
+//		 	type:'get',
+//		 	url:'http://datainfo.duapp.com/shopdata/userinfo.php',
+//		 	data:{status:'register',userID:this.name,password:this.password},
+//		 	dataType:'json',
+//		 	success:function(data){
+//		 		console.log(data);
+//		 	}
+//		 })
+	 })
